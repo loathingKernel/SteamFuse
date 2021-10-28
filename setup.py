1a3780df8ba6c926065df899c29a5793c1ea084b
Stelios Tsampas <loathingkernel@gmail.com>
2021-10-28 16:55:17 +0300

    Add base setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='steamfuse',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/loathingKernel/SteamFuse',
+    license='The Unlicense',
+    author='loathingkernel',
+    author_email='loathingkernel@gmail.com',
+    description='FUSE filesystem for the Steam Library'
+)
